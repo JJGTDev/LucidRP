8b7552a9c54d6cc4ca711a73566e60915d210aaa
JJGT <jjgthsrp@gmail.com>
2020-07-03 16:57:51 +0000

    Server push

---
+++ [Dev]/[Random]/jjgt-sec/__resource.lua
@@ -0,0 +1,124 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+description "Car Add-on"
+
+files {
+    'meta/2020ss/handling.meta',
+    'meta/2020ss/vehiclelayouts.meta',
+    'meta/2020ss/vehicles.meta',
+    'meta/2020ss/carcols.meta',
+    'meta/2020ss/carvariations.meta',
+    
+    'meta/buffalot/handling.meta',
+    'meta/buffalot/vehiclelayouts.meta',
+    'meta/buffalot/vehicles.meta',
+    'meta/buffalot/carcols.meta',
+    'meta/buffalot/carvariations.meta',
+    
+    'meta/demon/handling.meta',
+    'meta/demon/vehiclelayouts.meta',
+    'meta/demon/vehicles.meta',
+    'meta/demon/carcols.meta',
+    'meta/demon/carvariations.meta',
+    
+    'meta/er34/handling.meta',
+    'meta/er34/vehiclelayouts.meta',
+    'meta/er34/vehicles.meta',
+    'meta/er34/carcols.meta',
+    'meta/er34/carvariations.meta',
+    
+    'meta/fd/handling.meta',
+    'meta/fd/vehiclelayouts.meta',
+    'meta/fd/vehicles.meta',
+    'meta/fd/carcols.meta',
+    'meta/fd/carvariations.meta',
+    
+    'meta/fk2/handling.meta',
+    'meta/fk2/vehiclelayouts.meta',
+    'meta/fk2/vehicles.meta',
+    'meta/fk2/carcols.meta',
+    'meta/fk2/carvariations.meta',
+    
+    'meta/fk8/handling.meta',
+    'meta/fk8/vehiclelayouts.meta',
+    'meta/fk8/vehicles.meta',
+    'meta/fk8/carcols.meta',
+    'meta/fk8/carvariations.meta',
+    
+    'meta/ramsrt10/handling.meta',
+    'meta/ramsrt10/vehiclelayouts.meta',
+    'meta/ramsrt10/vehicles.meta',
+    'meta/ramsrt10/carcols.meta',
+    'meta/ramsrt10/carvariations.meta',
+    
+    'meta/raptor2017/handling.meta',
+    'meta/raptor2017/vehiclelayouts.meta',
+    'meta/raptor2017/vehicles.meta',
+    'meta/raptor2017/carcols.meta',
+    'meta/raptor2017/carvariations.meta',
+    
+    'meta/bentaygast/handling.meta',
+    'meta/bentaygast/vehiclelayouts.meta',
+    'meta/bentaygast/vehicles.meta',
+    'meta/bentaygast/carcols.meta',
+    'meta/bentaygast/carvariations.meta',
+}
+data_file 'HANDLING_FILE' 'meta/bentaygast/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/bentaygast/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/bentaygast/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/bentaygast/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/bentaygast/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/2020ss/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/2020ss/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/2020ss/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/2020ss/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/2020ss/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/buffalot/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/buffalot/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/buffalot/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/buffalot/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/buffalot/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/demon/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/demon/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/demon/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/demon/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/demon/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/er34/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/er34/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/er34/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/er34/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/er34/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/fd/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/fd/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/fd/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/fd/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/fd/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/fk2/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/fk2/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/fk2/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/fk2/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/fk2/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/fk8/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/fk8/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/fk8/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/fk8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/fk8/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/ramsrt10/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/ramsrt10/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/ramsrt10/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/ramsrt10/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/ramsrt10/carvariations.meta'
+
+data_file 'HANDLING_FILE' 'meta/raptor2017/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/raptor2017/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/raptor2017/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/raptor2017/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/raptor2017/carvariations.meta'
